78bf29156c2fbdf981f1f43b27d1b9c44d633776
Piotr Kowalski <piotr@pkowalski.net>
2022-07-05 15:46:17 +0200

    Add validation method for tag

--- Models/tag.go
+++ Models/tag.go
@@ -7,3 +7,13 @@ type Tag struct {
 	Name   string    `json:"name"`
 	UserId uuid.UUID `json:"userId"`
 }
+
+func (tag *Tag) IsValid() bool {
+	if tag.Name == "" {
+		return false
+	}
+	if tag.UserId == uuid.Nil {
+		return false
+	}
+	return true
+}
