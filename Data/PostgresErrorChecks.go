beebb86ce12d829ffe2b7b669a8df9130e26a23b
Piotr Kowalski <piotr@pkowalski.net>
2022-07-16 00:35:18 +0200

    Add error helpers

---
+++ Data/PostgresErrorChecks.go
@@ -0,0 +1,19 @@
+package Data
+
+import (
+	"errors"
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+)
+
+func IsPostgresDuplicateValue(err error) bool {
+	var pgErr *pgconn.PgError
+	if err == nil || !errors.As(err, &pgErr) {
+		return false
+	}
+	return pgErr.Code == "23505"
+}
+
+func IsPostgresNotFoundError(err error) bool {
+	return err != nil && err == pgx.ErrNoRows
+}
