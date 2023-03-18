30c10dc984a5fc5ef929a2084734a2c142ea0e56
Piotr Kowalski <piotr@pkowalski.net>
2023-03-18 10:35:48 +0100

    Add colour field to tag model

--- models/tag.go
+++ models/tag.go
@@ -5,14 +5,18 @@ import "github.com/gofrs/uuid"
 type Tag struct {
 	Id   uuid.UUID `json:"id" binding:"required"`
 	Name string    `json:"name" binding:"required,max=50"`
+	// Colour represents a visual tag identifier. Should be provided in HTML format, as in "#aabbcc".
+	Colour string `json:"colour" binding:"required,max=7"`
 }
 
 type TagCreateViewModel struct {
-	Name string `json:"name" binding:"required,max=50"`
+	Name   string `json:"name" binding:"required,max=50"`
+	Colour string `json:"colour" binding:"required,max=7"`
 }
 
 func (vm *TagCreateViewModel) GetTag() Tag {
 	return Tag{
-		Name: vm.Name,
+		Name:   vm.Name,
+		Colour: vm.Colour,
 	}
 }
